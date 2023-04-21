9024ddbd2f5b6536ed2ab42cf5c46ce007a4c789
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-04-21 18:21:56 +0200

    Feat: Creating pages

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-import express from "express";
+import express, { request, response } from "express";
 
-const app = express();
+const app = express()
+const port = 3002
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get('/', (request, response) => {
+    response.send("Hello World!")
     }
-);
+)
 
-app.listen(3002, () => {
-    console.log("Server running on port 3002");
-    console.log("http://localhost:3002");
+app.get('gallery', (request, response) => {
+    response.send('View the gallery')
+})
+
+app.get('about', (request, response) => {
+    response.send('About me and my work')
+})
+
+app.get('contact', (request, response) => {
+    response.send('A contact form including name, mail, phone, text, and a send button')
+})
+
+app.listen(port, () => {
+    console.log("Server running on port 3002")
+    console.log("http://localhost:3002")
     }
-);
+)

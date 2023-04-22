825fa4509808116054b4ef1b2f9a5a75a561cb2f
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-04-22 15:20:50 +0200

    Feat: pq-formula in Gallery page

--- app.js
+++ app.js
@@ -8,20 +8,24 @@ app.get('/', (request, response) => {
     }
 )
 
-app.get('gallery', (request, response) => {
-    response.send('View the gallery')
+app.get('/gallery', (request, response) => {
+    //response.send('View the gallery')
+    const y = `x^2 - 3x + 4`
+    const p = 3
+    const q = 1.5
+    response.send(`PQ-Formula of ${y} is calculated to x=${(-p)/2 + Math.sqrt((p/2)**2 - q)}`)
 })
 
-app.get('about', (request, response) => {
+app.get('/about', (request, response) => {
     response.send('About me and my work')
 })
 
-app.get('contact', (request, response) => {
+app.get('/contact', (request, response) => {
     response.send('A contact form including name, mail, phone, text, and a send button')
 })
 
 app.listen(port, () => {
-    console.log("Server running on port 3002")
-    console.log("http://localhost:3002")
+    console.log(`Server running on ${port}`)
+    console.log(`http://localhost:${port}`)
     }
 )

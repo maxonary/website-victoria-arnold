ce4d629bdb967281d341d81f3b99001d152c1e34
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-04-28 10:32:21 +0200

    Feat: Changing Navbar Font

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Victoria Arnold | Artist, Fashion Illustrator';
+export const SITE_DESCRIPTION = 'Berlin based artist with profession in Live Illustration and Events.';
